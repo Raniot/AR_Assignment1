1604ed61f3c51215e8ad7f62a7b9e0e3c05477a6
Anders Meidahl <Andersmeidahl@gmail.com>
2019-02-04 16:53:06 +0100

    First Part now working, with task c!

--- Assignment 1/Assets/Landing.cs
+++ Assignment 1/Assets/Landing.cs
@@ -15,48 +15,26 @@ public class Landing : MonoBehaviour
     void Update()
     {
         var spaceShuttleTarget = transform.Find("SpaceShuttleTarget");
-        var spaceShuttle = spaceShuttleTarget.Find("SpaceShuttle");
         var spaceShuttleMarking = spaceShuttleTarget.Find("Marking");
         var runwayTarget = transform.Find("RunwayTarget");
-        var runway = runwayTarget.Find("Runway");
 
-        var spaceShuttlePosition = spaceShuttle.position;
-        spaceShuttlePosition.z += 1;
+        var dotProductForward = Vector3.Dot(spaceShuttleTarget.forward, runwayTarget.forward);
+        var dotProductRight = Vector3.Dot(spaceShuttleTarget.right, runwayTarget.right);
 
+        Debug.LogError(nameof(dotProductForward) + dotProductForward);
+        Debug.LogError(nameof(dotProductRight) + dotProductRight);
 
-        //var crossProduct = Vector3.Cross(runway.position, spaceShuttle.position);
-        var test = new Vector2 {x = spaceShuttle.position.x, y = spaceShuttle.position.z};
-        var test2 = new Vector2 {x = runway.position.x, y = runway.position.z};
-        //var dotProduct = Vector3.Dot(spaceShuttle.position, runwayTarget.position);
-        var angle = Vector3.Angle(spaceShuttle.up, runway.up);
-        //var dotProduct = Vector3.Dot(spaceShuttle.position * spaceShuttle.lossyScale, runway.position.normalized);
-        var dotProduct = Vector2.Dot(spaceShuttle.position, runway.position);
+        var absDotProductForward = Math.Abs(dotProductForward);
+        var absDotProductRight = Math.Abs(dotProductRight);
 
-        Debug.Log(nameof(dotProduct) + dotProduct);
-        Debug.Log(nameof(angle) + angle);
+        const double margin = 0.92;
+        const double highMargin = 0.50;
 
-        var AbsDotProduct = Math.Abs(dotProduct);
-
-        if (AbsDotProduct <= 0) spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.green;
-        else if (AbsDotProduct <= 50) spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.blue;
-        else if (AbsDotProduct <= 100) spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.cyan;
-        else if (AbsDotProduct <= 200) spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.magenta;
+        if (absDotProductForward > margin && absDotProductRight > margin)
+            spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.green;
+        else if (absDotProductForward <= margin && absDotProductRight > highMargin || 
+                 absDotProductForward > highMargin && absDotProductRight <= margin) 
+            spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.yellow;
         else spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.red;
-
-
-        //if (dotProduct <= 0)
-        //{
-
-        //}
-        //else
-        //{
-        //    spaceShuttleMarking.GetComponent<Renderer>().material.color = Color.red;
-        //}
-        //runwayTarget
-        //transform.
-
-
-        //quad.
-        //var earth = sun.Find("Earth");
     }
 }
