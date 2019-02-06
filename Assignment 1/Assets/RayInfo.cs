2a78a305749db5a757db42cd79f4e31e7ca17baa
Nicklas Nielsen <nnicklas92@gmail.com>
2019-02-06 17:23:23 +0100

    Nicklas er Awesome

--- Assignment 1/Assets/RayInfo.cs
+++ Assignment 1/Assets/RayInfo.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace Assets
+namespace Models
 {
     class RayInfo
     {
--- Assignment 1/Assets/Shot.cs
+++ Assignment 1/Assets/Shot.cs
@@ -1,4 +1,4 @@
-﻿using Assets;
+﻿using Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,7 +22,7 @@ public class Shot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -30,22 +30,30 @@ public class Shot : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ShootTarget(transform.position, Color.green);
+            ShootTarget(transform.position, Color.blue);
         }
 
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            Vector3 localPosShip = transform.localPosition;
-            var worldPosShip = transform.parent.position + transform.parent.rotation * localPosShip;
-
             Matrix4x4 mat = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
 
-            var cannonLeftLocalPos = mat.MultiplyPoint3x4(new Vector3(-0.1f, 0f, 0.45f));
-            var cannonRightLocalPos = mat.MultiplyPoint3x4(new Vector3(0.1f, 0f, 0.45f));
+            var cannonLeftLocalPos = mat.MultiplyPoint3x4(new Vector3(-0.08f, 0f, 0.45f));
+            var cannonRightLocalPos = mat.MultiplyPoint3x4(new Vector3(0.08f, 0f, 0.45f));
 
             ShootTarget(cannonLeftLocalPos, Color.red);
             ShootTarget(cannonRightLocalPos, Color.red);
 
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+
+            Matrix4x4 mat = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+            var yValue = 0.025f / transform.lossyScale.y;
+
+            var cannonTopLocalPos = mat.MultiplyPoint3x4(new Vector3(0f, yValue, 0f));
+
+            ShootTarget(cannonTopLocalPos, Color.magenta);
         }
     }
 
@@ -64,6 +72,7 @@ public class Shot : MonoBehaviour
             GL.Vertex(ray.End);
             GL.End();
         }
+        _rays.Clear();
     }
 
     private IEnumerator ShotEffect()
@@ -77,21 +86,38 @@ public class Shot : MonoBehaviour
     {
         StartCoroutine(ShotEffect());
         Debug.Log("Shoot!!!");
-
+        var dir = Vector3.forward;
         var rayEnd = origin + transform.TransformDirection(Vector3.forward) * _rayLength;
 
-        _rays.Add(new RayInfo { Start = origin, End = rayEnd, Color = color });
+        if (color.Equals(Color.magenta))
+        {
+
+
+            var yaw = 45f;
+            var pitch = 10f;
+            var roll = 0f;
+            dir = Quaternion.Euler(pitch, yaw, roll) * Vector3.forward;
+
+            rayEnd = origin + transform.TransformDirection(dir) * _rayLength;
+        }
+
+
+        
 
         _ray.origin = origin;
-        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.forward), out var hit, Mathf.Infinity))
+        if (Physics.Raycast(origin, transform.TransformDirection(dir), out var hit, Mathf.Infinity))
         {
             Debug.Log("Did Hit");
-            Instantiate(Explosion, hit.point, Quaternion.Euler(0, 0, 0));
+            rayEnd = hit.point;
+            var clone = Instantiate(Explosion, hit.point, Quaternion.Euler(0, 0, 0));
             Destroy(Explosion);
+            Explosion = clone;
         }
         else
         {
             Debug.Log("Did not Hit");
         }
+
+        _rays.Add(new RayInfo { Start = origin, End = rayEnd, Color = color });
     }
 }
