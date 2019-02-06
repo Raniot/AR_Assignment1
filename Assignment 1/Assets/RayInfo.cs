dce47ac009ddd0f2d846c75159d92c0c225da312
Nicklas Nielsen <nnicklas92@gmail.com>
2019-02-06 15:32:43 +0100

    WooO!

---
+++ Assignment 1/Assets/RayInfo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    class RayInfo
+    {
+        public Vector3 Start { get; set; }
+        public Vector3 End { get; set; }
+        public Color Color { get; set; }
+    }
+}
